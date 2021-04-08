66935153e62fffeaeabc9c78e949694073de01f2
KoreZhong252 <77712486+zhonk252@users.noreply.github.com>
2021-04-08 13:26:54 +0800

    check 2

--- airtable.js
+++ airtable.js
@@ -36,7 +36,7 @@ function consoleLogLetters() {
 
 function showLetters() {
     console.log("showLetters()");
-    for(var i = 0; i < 50; i++){
+    for(var i = 0; i < 60; i++){
        var letteredImg = document.createElement("img");
       letteredImg.classList.add("tattoo-image");
       letteredImg.src = letters[i].fields.Attachments[0].url;
@@ -66,50 +66,56 @@ function showLetters() {
     
 }
 
-interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
+// interact('.draggable')
+//   .draggable({
+//     // enable inertial throwing
+//     inertia: true,
+//     // keep the element within the area of it's parent
    
-    // enable autoScroll
-    autoScroll: true,
+//     // enable autoScroll
+//     autoScroll: true,
 
-    listeners: {
-      // call this function on every dragmove event
-      move: dragMoveListener,
+//     listeners: {
+//       // call this function on every dragmove event
+//       move: dragMoveListener,
 
-      // call this function on every dragend event
-      end (event) {
-        var textEl = event.target.querySelector('p')
+//       // call this function on every dragend event
+//       end (event) {
+//         var textEl = event.target.querySelector('p')
 
-        textEl && (textEl.textContent =
-          'moved a distance of ' +
-          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px')
-      }
-    }
-  })
+//         textEl && (textEl.textContent =
+//           'moved a distance of ' +
+//           (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+//                      Math.pow(event.pageY - event.y0, 2) | 0))
+//             .toFixed(2) + 'px')
+//       }
+//     }
+//   })
 
-function dragMoveListener (event) {
-  var target = event.target
-  // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+// function dragMoveListener (event) {
+//   var target = event.target
+//   // keep the dragged position in the data-x/data-y attributes
+//   var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+//   var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)'
+//   // translate the element
+//   target.style.webkitTransform =
+//     target.style.transform =
+//       'translate(' + x + 'px, ' + y + 'px)'
 
 
-  // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
+//   // update the posiion attributes
+//   target.setAttribute('data-x', x)
+//   target.setAttribute('data-y', y)
   
   
-}
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
+// }
+// // this function is used later in the resizing and gesture demos
+// window.dragMoveListener = dragMoveListener
+
+
+var elem = document.querySelector('.draggable');
+var draggie = new Draggabilly( elem, {
+  // options...
+});
 
